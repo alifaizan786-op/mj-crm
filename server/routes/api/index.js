143877d83806790665a2b41287e6c11538f852d6
faizan Ali <faizan.ali@irunadigital.com>
2024-11-23 18:17:16 -0500

    remved utility controller and routes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const userRoutes = require('./User-routes');
 const userRoutes_Routes = require('./UserRoutes-routes');
-const utilityRoutes = require('./Utility-routes');
+
 
 // /api/user
 router.use('/user', userRoutes);
@@ -10,7 +10,6 @@ router.use('/user', userRoutes);
 // /api/userRoutes
 router.use('/userRoutes', userRoutes_Routes);
 
-// /api/utility
-router.use('/utility', utilityRoutes);
+
 
 module.exports = router;
