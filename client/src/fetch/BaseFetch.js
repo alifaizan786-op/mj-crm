401ff36ca0937a02ff1537151661addb27593f47
faizan Ali <faizan.ali@irunadigital.com>
2025-01-25 17:27:53 -0500

    updated base fetch to accept form data

--- client/src/fetch/BaseFetch.js
+++ client/src/fetch/BaseFetch.js
@@ -4,16 +4,15 @@ class BaseFetch {
     this.baseURL = baseURL;
   }
 
-  async request(
-    endpoint,
-    method = 'GET',
-    body = null,
-    customHeaders = {}
-  ) {
-    const headers = {
-      'Content-Type': 'application/json',
-      ...customHeaders,
-    };
+  async request(endpoint, method = 'GET', body = null, customHeaders = {}) {
+    const isFormData = body instanceof FormData;
+    
+    const headers = isFormData
+      ? customHeaders // No need to set 'Content-Type' for FormData; the browser sets it automatically
+      : {
+          'Content-Type': 'application/json',
+          ...customHeaders,
+        };
 
     const config = {
       method,
@@ -21,14 +20,11 @@ class BaseFetch {
     };
 
     if (body) {
-      config.body = JSON.stringify(body);
+      config.body = isFormData ? body : JSON.stringify(body);
     }
 
     try {
-      const response = await fetch(
-        `${this.baseURL}${endpoint}`,
-        config
-      );
+      const response = await fetch(`${this.baseURL}${endpoint}`, config);
       if (!response.ok) {
         throw new Error(`Error: ${response.statusText}`);
       }
